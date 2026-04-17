31f2d06adf1353e920b74cdbaf2a60b685935a6c
mranandkumaryadav16012-blip <mranandkumaryadav16012@gmail.com>
2026-04-17 08:21:49 +0530

    Refactor file_id handling and error checks

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 
 $BOT_TOKEN = "8653912920:AAGk2yd8fAThTL67CouDtj8gmlDTsQEOdyc";
 
-if(!isset($_GET['file_id'])){
-    echo "No file";
-    exit;
+$file_id = $_GET['file_id'] ?? '';
+
+if(!$file_id){
+    die("No file");
 }
 
-$file_id = $_GET['file_id'];
-
-// CURL request
+// Telegram API
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot$BOT_TOKEN/getFile?file_id=$file_id");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -18,11 +17,6 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 $res = json_decode($response, true);
-
-if(!$res || !isset($res['result']['file_path'])){
-    echo "Error";
-    exit;
-}
 
 $file_path = $res['result']['file_path'];
 
